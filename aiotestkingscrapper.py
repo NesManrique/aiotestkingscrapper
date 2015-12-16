2e98fe8f5ce3cd836d2c82418381cb46a23256f7
XxNocturxX <nes.manrique@gmail.com>
2015-12-16 14:09:41 -0500

    Working on adding image support

--- aiotestkingscrapper.py
+++ aiotestkingscrapper.py
@@ -33,6 +33,8 @@ from PIL import Image
 import urllib.request
 import re
 from io import BytesIO
+import functools
+import binascii
 
 def get_image_from_url(image_url):
     url_contents = urllib.request.urlopen(image_url)
@@ -41,24 +43,30 @@ def get_image_from_url(image_url):
     return im
 
 def image_to_hex(image, format):
-    output = BytesIO()
-    image.save(output, format=format)
-    hex_string=output.getvalue()
-    return hex_string
+    #output = BytesIO()
+    #image.save(output, format=format)
+    #byte_string=output.getvalue()
+    #print(binascii.hexlify(byte_string.encode('utf-8')))
+    #return binascii.hexlify(byte_string.encode('utf-8'))
 
-def question_parser_rtf(question):
+    img_bytes=image.tobytes(encoder_name='ascii')
+    #print(binascii.hexlify(img_bytes))
+    return binascii.hexlify(img_bytes)
+
+def question_parser_rtf(question,img_support):
     # Get question paragraphs (question and options)
     q_options =  question.find_all('p')
 
-    # Get answers for the question from the rest paragraphs tags
-    answers = 'Answers: '
-    for x in q_options[1:]:
+    # Get question, options and answers from paragraphs
+    parsed_q=''
+    answers = 'Ans: '
+    for x in q_options:
         xtext = x.get_text()
-        parsed_q=''
 
-        # If it is an Explanation dont add it to the final string 
+        # If it is an Explanation dont add it to the final string also
+        # don't check the rest of the paragraphs in this question
         if xtext[:11] == 'Explanation':
-            continue
+            break
 
         if re.match('[A-H]\.', xtext[:2]):
             # This is an option so add it to the final string with a newline
@@ -72,19 +80,24 @@ def question_parser_rtf(question):
             # This must be part of the question statement so add it to the string
             parsed_q = parsed_q+xtext+"\line\par\n"
 
-            # Check if there is an image with this paragraph of the question
-            img_tag = x.find('img')
-            if img_tag:
-                # Get image from URL
-                image = get_image_from_url(img_tag['src'])
-                image_hex = image_to_hex(image,image.format)
+            # If image support flag is enable, process images
+            if img_support:
+                # Check if there is an image with this paragraph of the question
+                img_tag = x.find('img')
+                if img_tag:
+                    # Get image from URL
+                    image = get_image_from_url(img_tag['src'])
+                    image_hex = image_to_hex(image,image.format)
 
-                print(str(image.format) + " " + str(image.width) + " " + str(image.height) +"\n")
-                print(img_tag['src'])
+                    #print(str(image.format) + " " + str(image.width) + " " + str(image.height) +"\n")
+                    #print(img_tag['src'])
 
-                parsed_q = parsed_q + "{\pict\jpegblip\picw"+str(image.width)+"\pich"+str(image.height)+" "+str(image_hex)+"}\line\par\n"
-                #parsed_q = parsed_q + "{\pict\jpegblip\picw"+str(image.width)+"\pich"+str(image.height)+" "+str(image.tobytes())+"}\line\par\n"
-                
+                    parsed_q = parsed_q + "{\pict\jpegblip\picw" + str(image.width) + \
+                        "\pich" + str(image.height) + "\picwgoal" + str(image.width) + \
+                        "\pichgoal" + str(image.height) + \
+                        " " + str(image_hex) + "}\line\par\n"
+
+                    #parsed_q = parsed_q + "{\pict\jpegblip\picw"+str(image.width)+"\pich"+str(image.height)+" "+str(image.tobytes())+"}\line\par\n"
 
     parsed_q = parsed_q+answers[:-2]+'\line\par\n'
     return parsed_q
@@ -92,27 +105,40 @@ def question_parser_rtf(question):
 def question_parser_txt(question):
     q_options =  question.find_all('p')
     parsed_q = q_options[0].get_text()+"\n"
-    answers = 'Answers: '
+    answers = 'Ans: '
     for x in q_options[1:]:
         if x.get_text()[:11] == 'Explanation':
-            continue
+            break
         parsed_q = parsed_q+x.get_text()+"\n"
         if x.find('font', color='#333333'):
             answers=answers+x.contents[0][:-1]+', '
+    parsed_q = parsed_q+answers[:-2]+'\n'
+    return parsed_q
 
 
 parser = ArgumentParser(description="Generates rtf with questions from aiotestking.")
 parser.add_argument('exam_code', help='Code of the exam and version (i.e. "1z0-821", "1z0-821 (v.2)")')
 parser.add_argument('-f', '--output-filepath', help='especify output file name', metavar='output_filepath')
+parser.add_argument('-i', '--add-images', help='enable adding images to rtf file', action='store_true')
 parser.add_argument('-txt', '--txt-output', action='store_true', help='outputs txt format instead of rtf (warning, txt format does not support images in questions)')
 
 args = parser.parse_args()
 
 exam_code=args.exam_code
-output_filepath=exam_code+'.rtf'
+image_support=args.add_images
+txt_output=args.txt_output
 
-if args.output_filepath and args.output_filepath[-4] == '.' and args.output_filepath[3:] != '.rtf':
+if txt_output:
+    output_filepath=exam_code+'.txt'
+else:
+    output_filepath=exam_code+'.rtf'
+
+if txt_output and args.output_filepath and args.output_filepath[-4] == '.' and args.output_filepath[3:] != '.txt':
+    output_filepath=args.output_filepath[:-4] + '.txt'
+
+if not txt_output and args.output_filepath and args.output_filepath[-4] == '.' and args.output_filepath[3:] != '.rtf':
     output_filepath=args.output_filepath[:-4] + '.rtf'
+    
 
 url_home="http://www.aiotestking.com"
 
@@ -138,13 +164,17 @@ print("The test has "+total_pages+" pages of questions...\n")
 
 q_number=0
 
-file.write("{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fswiss\\fcharset0 Arial;}}\n")
+if not txt_output:
+    file.write("{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fswiss\\fcharset0 Arial;}}\n")
 
 for i in range(2, int(total_pages)+2):
     questions = soup.find_all('div', class_='archive_post_block')
-    for n,q in zip(range(q_number+1,len(questions)+q_number+1),map(question_parser_rtf,questions)):
-        file.write(str(n)+'. '+q+'\line\par\n')
-        #print(str(n)+'. '+q)
+    if txt_output:
+        for number,question in zip(range(q_number+1,len(questions)+q_number+1),map(question_parser_txt,questions)):
+            file.write(str(number)+'. '+question+'\n')
+    else:
+        for n,q in zip(range(q_number+1,len(questions)+q_number+1),map(functools.partial(question_parser_rtf,img_support=image_support),questions)):
+            file.write(str(n)+'. '+q+'\line\par\n')
 
     q_number = q_number+len(questions)
 
@@ -154,6 +184,8 @@ for i in range(2, int(total_pages)+2):
 
     soup = BeautifulSoup(next_page.read(), 'html.parser')
 
-file.write("}}\n\x00")
+if not txt_output:
+    file.write("}}\n\x00")
+
 file.close()
 print("Questions saved in "+output_filepath)
